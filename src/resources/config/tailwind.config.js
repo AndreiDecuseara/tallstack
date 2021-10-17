4286c6bdfc28d54364d6f235f66169ee87d3d582
Andrei Decuseara <andrei.decuseara@updivision.com>
2021-10-17 14:50:53 +0300

    Udpate

---
+++ src/resources/config/tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: [
+    './app/**/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.html',
+    './resources/**/*.js',
+    './resources/**/*.jsx',
+    './resources/**/*.ts',
+    './resources/**/*.tsx',
+    './resources/**/*.php',
+    './resources/**/*.vue',
+    './resources/**/*.twig',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
