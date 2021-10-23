2e5258f4f606be80de1f12c092533ff9282808b8
Andrei Decuseara <andrei.decuseara@updivision.com>
2021-10-23 17:42:38 +0300

    New files distributions

--- src/TallStack.php
+++ src/TallStack.php
@@ -17,57 +17,18 @@ class TallStack extends Preset
      */
     public static function install()
     {
-        static::updatePackages();
         
-        static::makePackage();
+        $filesystem = new Filesystem();
+
+        $filesystem->copyDirectory(__DIR__ . '/../stubs/default', base_path());
         
     }
 
-    /**
-     * Update the given package array.
-     *
-     * @param  array  $packages
-     * @return array
-     */
-    protected static function updatePackageArray(array $packages)
+    public static function installAuth()
     {
-        return $packages;
-    }
+        $filesystem = new Filesystem();
 
-    /**
-     * Update the default welcome page file.
-     *
-     * @return void
-     */
-    protected static function makePackage()
-    {
-
-
-        // Copy css
-        static::copyFile('css/app.css', resource_path('css'), 'app.css');
-        static::copyFile('css/custom.css',  resource_path('css'),'custom.css');
-
-        static::copyFile('js/app.js', resource_path('js'), 'app.js');
-        static::copyFile('js/custom.js',  resource_path('js'),'custom.js');
-        
-        static::copyFile('config/tailwind.config.js', '.' ,'tailwind.config.js');
-
-        static::copyFile('config/webpack.mix.js', '.' ,'webpack.mix.js');
-
-        // just demo page
-        static::copyFile('config/particles.json', public_path('assets') ,'particles.json');
-
-        static::copyFile('Livewire/Home.php', app_path('Http/Livewire'),'Home.php');
-        static::copyFile('Livewire/Layouts/Navbar.php', app_path('Http/Livewire/Layouts'),'Navbar.php');
-        static::copyFile('Livewire/Layouts/Footer.php', app_path('Http/Livewire/Layouts'),'Footer.php');
-
-        static::copyFile('views/layouts/app.blade.php', resource_path('views/layouts'),'app.blade.php');
-        static::copyFile('views/livewire/home.blade.php', resource_path('views/livewire'),'home.blade.php');
-        static::copyFile('views/livewire/layouts/navbar.blade.php', resource_path('views/livewire/layouts'),'navbar.blade.php');
-        static::copyFile('views/livewire/layouts/footer.blade.php', resource_path('views/livewire/layouts'),'footer.blade.php');
-        
-        static::copyFile('config/web.php', './routes' ,'web.php');
-        
+        $filesystem->copyDirectory(__DIR__ . '/../stubs/auth', base_path());
     }
 
         /**
@@ -97,15 +58,4 @@ class TallStack extends Preset
         (new Filesystem)->copy(static::STUBSPATH.$file, $destination);
     }
 
-    /**
-     * Copy a directory
-     *
-     * @param string $directory
-     * @param string $destination
-     * @return void
-     */
-    private static function copyDirectory($directory, $destination)
-    {
-        (new Filesystem)->copyDirectory(static::STUBSPATH.$directory, $destination);
-    }
 }
