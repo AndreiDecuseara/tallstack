fc5d951d3738b164ca7eb713bcb6bb98e670f250
Andrei Decuseara <andrei.decuseara@updivision.com>
2021-11-01 11:54:12 +0200

    Update

--- stubs/auth/resources/views/livewire/layouts/navbar.blade.php
+++ stubs/auth/resources/views/livewire/layouts/navbar.blade.php
@@ -1,11 +1,23 @@
-<nav class="h-14 px-4 py-8">
+<nav class="px-4 py-8 h-14">
   @auth
-    
+    <div class="float-right pr-4">
+        <a
+        href="{{ route('logout') }}"
+        onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+        class="px-6 py-3 mr-4 text-white bg-green-600 rounded-lg shadow-xl"
+        >
+        Log out
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+        </form>
+    </div>
   @endauth
   @guest
   <div class="float-right pr-4">
-    <a class="px-6 py-3 bg-green-600 text-white rounded-lg mr-4 shadow-xl" href="/login">Login</a>
-    <a class="px-6 py-3 bg-green-600 text-white rounded-lg shadow-xl" href="/register">Register</a>
+    <a class="px-6 py-3 mr-4 text-white bg-green-600 rounded-lg shadow-xl" href="/login">Login</a>
+    <a class="px-6 py-3 text-white bg-green-600 rounded-lg shadow-xl" href="/register">Register</a>
   </div>
   @endguest
 </nav>
