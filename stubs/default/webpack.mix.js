218a27354c94858f1d5af7d7a28bf864cb0392ec
Andrei Decuseara <andrei.decuseara@updivision.com>
2022-01-02 16:15:35 +0200

    Update css to scss

--- stubs/default/webpack.mix.js
+++ stubs/default/webpack.mix.js
@@ -12,9 +12,7 @@ const tailwindcss = require('tailwindcss');
  |
  */
 
- mix.js('resources/js/app.js', 'public/js')
+ mix .js('resources/js/app.js', 'public/js')
     .sass('resources/scss/app.scss', 'public/css')
-    .options ({
-        postCss: [ tailwindcss('./tailwind.config.js') ],
-    })
+    .tailwind()
     .version();
